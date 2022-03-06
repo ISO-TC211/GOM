2a889d7d0cfc6cdc4f3fad2c9ce38087ed986f85
nicholascar <nicholas.car@surroundaustralia.com>
2022-03-06 11:21:29 +1000

    Validate & upload all valid vocabs

--- experimental/scripts/extract_cses.py
+++ experimental/scripts/extract_cses.py
@@ -10,8 +10,8 @@ tc_graph.add((tc, SDO.description, Literal("The International Organization for S
 tc_graph.add((tc, SDO.url, Literal("https://www.iso.org/committee/54904.html", datatype=XSD.anyURI)))
 
 
-ont_dir = Path(__file__).parent.parent.parent.parent / "isotc211_GOM_harmonizedOntology" / "iso19115" / "-1" / "2018"
-voc_dir = Path(__file__).parent.parent.parent.parent / "experimental" / "codelist-vocabularies" / "iso19115" / "-1" / "2018"
+ont_dir = Path(__file__).parent.parent.parent / "isotc211_GOM_harmonizedOntology" / "iso19115" / "-1" / "2018"
+voc_dir = Path(__file__).parent.parent.parent / "experimental" / "codelist-vocabularies" / "iso19115" / "-1" / "2018"
 voc_dir.mkdir(parents=True, exist_ok=True)
 
 rdf_files = ont_dir.glob("*.rdf")
@@ -27,21 +27,24 @@ for cs in g.subjects(RDF.type, SKOS.ConceptScheme):
     g2.bind("cs", cs)
     g2.bind("", Namespace(str(cs) + "/"))
     for s, p, o in g.triples((cs, None, None)):
-        g2.add((s, p, o))
         if p == RDFS.isDefinedBy:
             g2.add((cs, DCTERMS.source, Literal(str(o), datatype=XSD.anyURI)))
             g2.remove((cs, RDFS.isDefinedBy, o))
         elif p == SKOS.prefLabel:
             g2.add((cs, p, Literal(str(o).replace(" - ConceptScheme", ""), lang="en")))
+        else:
+            g2.add((s, p, o))
     g2.add((cs, SKOS.definition, Literal("An ISO standard's ConceptScheme", lang="en")))
 
     # CS metadata
     g2.add((cs, DCTERMS.creator, tc))
-    g2.add((cs, DCTERMS.publisher, tc))
     g2.add((cs, DCTERMS.created, Literal("2000-01-01", datatype=XSD.date)))
+    g2.add((cs, DCTERMS.publisher, tc))
     g2.add((cs, DCTERMS.modified, Literal("2000-01-01", datatype=XSD.date)))
     g2.add((cs, DCTERMS.issued, Literal("2000-01-01", datatype=XSD.date)))
     g2.add((cs, DCTERMS.provenance, Literal("Derived from TC211-produced RDF versions of the standard's UML", lang="en")))
+    g2.add((cs, DCTERMS.identifier, Literal(str(cs).split("/")[-1], datatype=XSD.token)))
+
     g2 += tc_graph
 
     for c in g.subjects(SKOS.inScheme, cs):
@@ -55,8 +58,12 @@ for cs in g.subjects(RDF.type, SKOS.ConceptScheme):
                     g2.add((s2, p2, o2))
         g2.add((c, DCTERMS.provenance, Literal("Code originally presented in the standard", lang="en")))
         g2.add((c, SKOS.topConceptOf, cs))
+        g2.add((c, DCTERMS.identifier, Literal(str(c).split("/")[-1], datatype=XSD.token)))
 
         g2.add((cs, SKOS.hasTopConcept, c))
+
+        if (c, SKOS.definition, None) not in g2:
+            g2.add((c, SKOS.definition, Literal("Missing", lang="en")))
 
     g2.serialize(destination=Path(voc_dir) / f"{str(cs).split('/')[-1]}.ttl", format="longturtle")
 
---
+++ experimental/scripts/upload_vocabs.py
@@ -0,0 +1,76 @@
+from typing import List, Union
+import json
+import os
+import time
+import argparse
+from pathlib import Path
+import httpx
+from rdflib import Graph
+from rdflib.namespace import RDF, SKOS
+from pyshacl import validate
+
+
+def validate_vocab(vocab: Union[Path, Graph]):
+    val = Graph().parse(Path(__file__).parent / "vocpub-validator.ttl")
+    val.parse(Path(__file__).parent / "vocprez-additions-validator.ttl")
+    if type(vocab) == Path:
+        vocab = Graph().parse(vocab)
+    return validate(vocab, shacl_graph=val, allow_warnings=True)
+
+
+def validate_all_vocabs():
+    for vocab in sorted(list_all_vocabs()):
+        conforms, results_graph, results_text = validate_vocab(vocab)
+        if not conforms:
+            print(f"{vocab.name} is invalid")
+            # print(results_text)
+
+
+def upload_vocab(p: Path):
+    g = Graph().parse(p)
+    v = validate_vocab(g)
+    if v[0]:
+        print(f"Uploading {p.name}")
+        iri = None
+        for cs in g.subjects(RDF.type, SKOS.ConceptScheme):
+            iri = str(cs)
+
+        r = httpx.post(
+            DB_ENDPOINT + "/data",
+            params={"graph": iri},
+            headers={"Content-Type": "text/turtle"},
+            content=p.read_bytes(),
+            auth=(DB_USERNAME, DB_PASSWORD),
+        )
+
+        assert 200 <= r.status_code <= 300, "Status code was {}".format(r.status_code)
+    else:
+        print(f"Skipping {p.name}, invalid")
+
+
+def upload_all_vocabs():
+    for vocab in list_all_vocabs():
+        upload_vocab(vocab)
+
+
+def drop_all_vocabs():
+    r = httpx.post(DB_ENDPOINT, data={"update": "DROP ALL"}, auth=(DB_USERNAME, DB_PASSWORD))
+
+    assert 200 <= r.status_code <= 300, "Status code was {}".format(r.status_code)
+
+
+def list_all_vocabs():
+    vs = []
+    for v in Path(Path(__file__).parent.parent / "codelist-vocabularies").rglob("*.ttl"):
+        vs.append(v)
+    return vs
+
+
+if __name__ == "__main__":
+    # validate_all_vocabs()
+    DB_ENDPOINT = os.environ.get("DB_ENDPOINT", None)
+    DB_USERNAME = os.environ.get("DB_USERNAME", None)
+    DB_PASSWORD = os.environ.get("DB_PASSWORD", None)
+
+    drop_all_vocabs()
+    upload_all_vocabs()
